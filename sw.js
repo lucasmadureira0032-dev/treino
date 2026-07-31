03c6211b47085bde04e9c1c0f742294f683c3ef2
Lucas Madureira <lucas.madureira0032@gmail.com>
2026-07-31 19:46:47 -0300

    Corrige botao Sair: logout local nao depende de rede

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guia-treino-v7';
+const CACHE = 'guia-treino-v8';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
