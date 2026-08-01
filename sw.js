f08a7c1ed37b65f110a6ec4e99e676ecce35ea4f
Lucas Madureira <lucas.madureira0032@gmail.com>
2026-07-31 21:08:33 -0300

    Sair: limpa sessao do localStorage manualmente antes de recarregar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guia-treino-v8';
+const CACHE = 'guia-treino-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
