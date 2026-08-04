96bd67fe3c1054b9f53b031430ff9f3f96e6814f
Lucas Madureira <lucas.madureira0032@gmail.com>
2026-08-03 21:10:36 -0300

    Sair a prova de falha (flag loggedOut) + SW auto-update + versao visivel v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guia-treino-v10';
+const CACHE = 'guia-treino-v11';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
