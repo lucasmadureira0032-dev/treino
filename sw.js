823bdc811ec5dba287a114e4a28e623abc956a3d
Lucas Madureira <lucas.madureira0032@gmail.com>
2026-08-03 21:23:28 -0300

    Data da viagem para Jeri: 02/01/2027 v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guia-treino-v12';
+const CACHE = 'guia-treino-v13';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
