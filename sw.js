f93a4cd760b3d07e466b841342d4a920234c36ea
Lucas Madureira <lucas.madureira0032@gmail.com>
2026-08-03 21:25:38 -0300

    Usa data local (fuso do Brasil) em vez de UTC no todayKey v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guia-treino-v13';
+const CACHE = 'guia-treino-v14';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
