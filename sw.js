fb2c71e2b131ecb3aa250817a1de8a92b29622f5
Lucas Madureira <lucas.madureira0032@gmail.com>
2026-08-03 21:17:31 -0300

    Corrige toque no botao Sair: circulo decorativo do header interceptava o clique (pointer-events:none) v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guia-treino-v11';
+const CACHE = 'guia-treino-v12';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
